2fe2ef3154ab36d9a3a051bd73c17ae493c6ab2a
Luis Felipe <37634205+ludovici-philippus@users.noreply.github.com>
2024-03-09 19:50:47 -0300

    Updated Convert
    Now it's possible to convert multiple files at once

--- convert.py
+++ convert.py
@@ -13,19 +13,40 @@ def colorize(text, color):
 def check_params():
     if(len(sys.argv) < 3):
         print(colorize("[ERROR] You must use at least 2 params!", 'red'))
-        print("Usage: ./convert.py [PATH_TO_FILE] [FORMAT] [QUALITY (optional)]")
+        print("Usage: ./convert.py [PATH_TO_FILE [FILE_01] [FILE_02] [FILE_03]] [FORMAT] [QUALITY (optional)]")
         quit()
 
 def get_filename(img):
     img_name = img.filename.split('\\')[-1].split('/')[-1].split('.')[0]
     return img_name
 
+def last_argument_is_quality():
+    return sys.argv[-1].isdigit()
+
+def get_type_to_convert():
+    if(last_argument_is_quality()):
+        return sys.argv[-2].lower()
+    return sys.argv[-1].lower()
+
+def get_files_to_convert():
+    return sys.argv[1:-2] if last_argument_is_quality() else sys.argv[1:-1]
+
+def get_quality():
+    return int(sys.argv[-1]) if last_argument_is_quality() else 95
+
+def convert(files):
+    for file in files:
+        img = Image.open(file)
+        img.convert("RGB").save(f"{get_filename(img)} - Converted.{TYPE_TO_CONVERT}", TYPE_TO_CONVERT, optimize=True, quality=QUALITY)
+
 check_params()
 
-BASE_FILE = sys.argv[1]
-TYPE_TO_CONVERT = sys.argv[2].lower() if sys.argv[2].lower() != 'jpg' else 'jpeg'
-QUALITY = int(sys.argv[3]) if len(sys.argv) > 3 else 95
+FILES_TO_CONVERT = get_files_to_convert()
+TYPE_TO_CONVERT = get_type_to_convert() if get_type_to_convert() != 'jpg' else 'jpeg'
+QUALITY = get_quality()
+convert(FILES_TO_CONVERT)
 
-img = Image.open(BASE_FILE)
-img.convert("RGB").save(f"{get_filename(img)} - Converted.{TYPE_TO_CONVERT}", TYPE_TO_CONVERT, optimize=True, quality=QUALITY)
-print(colorize("Image Converted!", 'yellow'))
+if(len(FILES_TO_CONVERT) == 1):
+    print(colorize("Image Converted!", 'yellow'))
+else:
+    print(colorize("Images Converted!", 'yellow'))
